debb6c4ad30d5e37c41d1d13a116a4b295f72dc7
Wesley.antunesdc <wesleyantunesdca@gmail.com>
2021-03-05 14:27:53 -0300

    Criação de uma classe para a implementação do asyncTask

--- app/src/main/java/wesleyantunes/cursoandroid/threads/entendendofuncoesassincronas/MainActivity.java
+++ app/src/main/java/wesleyantunes/cursoandroid/threads/entendendofuncoesassincronas/MainActivity.java
@@ -3,12 +3,27 @@ package wesleyantunes.cursoandroid.threads.entendendofuncoesassincronas;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.ProgressBar;
 
 public class MainActivity extends AppCompatActivity {
+    private ProgressBar progressBar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        progressBar = findViewById(R.id.progressBar);
+    }
+
+    public void iniciarAsyncTask(View view){
+        faca("Jamilton","Welsey","Teste");
+    }
+
+
+    public void faca(String... string){
+        String nome = string[0];
+        Log.d("Executar", "msg: "+ nome);
     }
 }
---
+++ app/src/main/java/wesleyantunes/cursoandroid/threads/entendendofuncoesassincronas/MyAsyncTask.java
@@ -0,0 +1,16 @@
+package wesleyantunes.cursoandroid.threads.entendendofuncoesassincronas;
+
+import android.os.AsyncTask;
+/*
+1 - Parâmetro a ser passado para a classe/Void
+2 - Tipo valor que será utilizado para o progresso da tarefa
+3 - Retorno após tarefa finalizada
+ */
+
+public class MyAsyncTask extends AsyncTask<String,Integer, String> {
+
+    @Override
+    protected String doInBackground(String... strings) {
+        return null;
+    }
+}
